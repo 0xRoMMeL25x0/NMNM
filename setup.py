2e75a1b5d9016b29f97f03ed898da267617b550b
NGM-3001 <nagem5534@gmail.com>
2024-06-25 13:41:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ try :
     import json
     import datetime
     from bs4 import BeautifulSoup
+    print("Ok All requirements is Installed")
 except ImportError:
     platform = sys.platform
     if platform == "win32":
         os.system("pip install pyfiglet termcolor requests json beautifulsoup4")
+        print("Ok All requirements is Installed")
     else :
         os.system("pip3 install pyfiglet termcolor requests json beautifulsoup4")
+        print("Ok All requirements is Installed")
         
